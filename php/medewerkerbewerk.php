696223056450958d4b3f55f9837b0b4d8d139d37
Dion Muller <00305891@rijnijssel.nl>
2021-10-04 08:48:04 +0200

    updated fo and medewerker bewrk

--- php/medewerkerbewerk.php
+++ php/medewerkerbewerk.php
@@ -8,7 +8,29 @@ $email      = $_POST['email'];
 $id         = $_POST['id'];
 $wachtwoord = $_POST['wachtwoord'];
 
-$sql = "UPDATE medewerkers
+$sql = "SELECT *
+FROM medewerkers
+WHERE email = :email";
+$stmt = $conn->prepare($sql);
+$stmt->execute(array(
+    ':email'    => $email
+));
+
+$rowCount = $stmt->rowCount();
+
+if($rowcount > 0)
+{
+    $_SESSION['error'] = "email bestaat al";
+    header('location: ../index.php?page=registreren');
+}
+
+else if(strlen($wachtwoord) < 6)
+{
+    $_SESSION['error'] = "Wachtwoord is tekort, moet minimaal 6 karakters hebben";
+    header('location: ../index.php?page=registreren');
+}
+
+$sql2 = "UPDATE medewerkers
 
 SET voornaam    = :voornaam,
 achternaam      = :achternaam,
@@ -17,8 +39,8 @@ wachtwoord      = :wachtwoord
 
 WHERE medewerker_id = :id";
 
-$stmt = $conn->prepare($sql);
-$result = $stmt->execute(array(
+$stmt2 = $conn->prepare($sql2);
+$result = $stmt2->execute(array(
     ':voornaam'     => $voornaam,
     ':achternaam'   => $achternaam,
     ':email'        => $email,
