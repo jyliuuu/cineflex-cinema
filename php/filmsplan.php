bed587c60517ec2bc8327b9e4f6ac53dcb90058e
Acer Laptop <00304501@rijnijssel.nl>
2021-10-12 16:19:51 +0200

    Cineflex v0.81

--- php/filmsplan.php
+++ php/filmsplan.php
@@ -5,8 +5,15 @@ require "../private/connectioncineflex.php";
 $filmid = $_POST['film'];
 $zaal = $_POST['zaal'];
 $starttijd = $_POST['start'];
-$duratie = $_POST['duratie'];
 $datum = $_POST['datum'];
+
+$sql0 = "SELECT duratie FROM films WHERE film_id = :film_id";
+$smt0 = $conn->prepare($sql0);
+$smt0->execute(array(
+    ':film_id' => $filmid
+));
+$r0 = $smt0->fetch(PDO::FETCH_ASSOC);
+$duratie = $r0['duratie'];
 
 // for time converts
 $hours = floor($duratie / 60);
@@ -21,8 +28,8 @@ echo $Date;
 
     $sql = "SELECT *
             FROM planning
-            WHERE datum = :datum AND zaal_id = :zaalid AND (:begintijd >= begin_tijd AND :begintijd <= eind_tijd)
-            OR :eindtijd >= begin_tijd AND :eindtijd <= eind_tijd"; 
+            WHERE datum = :datum AND zaal_id = :zaalid AND ((:begintijd >= begin_tijd AND :begintijd <= eind_tijd)
+            OR (:eindtijd >= begin_tijd AND :eindtijd <= eind_tijd))";
     $smt = $conn->prepare($sql);
     $smt->execute(array(
         ':zaalid' => $zaal,    
@@ -32,9 +39,9 @@ echo $Date;
     ));
     $r = $smt->fetchAll(PDO::FETCH_ASSOC);
 
-
     if ($smt->rowCount() == 0) 
     {
+        echo "hoi";
         $sql4 = "INSERT INTO planning (film_id, begin_tijd, eind_tijd, zaal_id, datum) 
         VALUE (:film_id, :begin_tijd, :eind_tijd, :zaal_id, :datum)";
         $smt4 = $conn->prepare($sql4);
@@ -45,10 +52,11 @@ echo $Date;
             ':zaal_id' => $zaal,
             ':datum' => $datum
         ));
-        header('location: ../index.php?page=filmsplanning');
+//        header('location: ../index.php?page=filmsplanning');
     }
     else
     {
+        echo "doei";
         $_SESSION['error'] = "IS AL VOL LMAO";
-        header('location: ../index.php?page=filmsplanning');
+//        header('location: ../index.php?page=filmsplanning');
     }
